7cc539f26a74d865210017dbe9e7a3de5e85cf20
jengmun <wongjm4@hotmail.com>
2022-05-13 17:45:45 +0800

    ** need to update

--- frontend/src/pages/Tasks/CreateApp.js
+++ frontend/src/pages/Tasks/CreateApp.js
@@ -2,6 +2,16 @@ import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import handleGetRequest from "../../hooks/handleGetRequest";
 import handlePostRequest from "../../hooks/handlePostRequest";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextareaAutosize,
+  TextField,
+} from "@mui/material";
 
 const CreateApp = () => {
   // ------------- Fetch all groups -------------
@@ -47,59 +57,78 @@ const CreateApp = () => {
 
   return (
     <>
-      <NavLink to="/">
-        <button>Back</button>
+      <NavLink to="/" style={{ textDecoration: "none" }}>
+        <Button>Back</Button>
       </NavLink>
-      <form onSubmit={handleCreateApp}>
-        <label htmlFor="acronym">Application Acronym</label>
-        <input
+      <Box
+        onSubmit={handleCreateApp}
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <TextField
+          required
+          InputProps={{ inputProps: { minLength: 3, maxLength: 3 } }}
           id="acronym"
-          name="acronym"
-          minLength="3"
-          maxLength="3"
-          required
+          label="Application Acronym"
+          sx={{ mt: 1, mb: 1 }}
         />
-        <label htmlFor="description">Description</label>
-        <textarea
-          id="description"
-          name="description"
+        <TextareaAutosize
+          required
           maxLength="255"
-          required
+          minRows={3}
+          id="description"
+          placeholder="Description"
+          sx={{ mt: 1, mb: 1 }}
         />
-        <label htmlFor="startDate">Start Date</label>
-        <input
-          type="date"
+        <TextField
           id="startDate"
-          name="startDate"
+          label="Start Date"
+          variant="outlined"
+          type="date"
           required
           onChange={(e) => {
             setStartDate(e.target.value);
           }}
+          defaultValue={new Date().toISOString().slice(0, 10)}
+          sx={{ mt: 2 }}
         />
-        <label htmlFor="endDate">End Date</label>
-        <input
-          type="date"
+        <TextField
           id="endDate"
-          name="endDate"
+          label="End Date"
+          variant="outlined"
           required
-          min={startDate}
+          type="date"
+          InputProps={{ inputProps: { min: startDate } }}
+          defaultValue={new Date().toISOString().slice(0, 10)}
+          sx={{ mt: 2 }}
         />
         {permissions.map((permission) => {
           return (
-            <>
-              <label htmlFor={permission}>{permission}</label>
-              <select name={permission} id={permission}>
+            <FormControl>
+              <InputLabel id={permission}>{permission}</InputLabel>
+              <Select
+                id={permission}
+                labelId={permission}
+                label={permission}
+                sx={{ width: "min-content" }}
+              >
                 {allGroups.map((group) => {
                   return (
-                    <option value={group.group_name}>{group.group_name}</option>
+                    <MenuItem value={group.group_name}>
+                      {group.group_name}
+                    </MenuItem>
                   );
                 })}
-              </select>
-            </>
+              </Select>
+            </FormControl>
           );
         })}
-        <button>Submit</button>
-      </form>
+        <Button type="submit">Submit</Button>
+      </Box>
     </>
   );
 };
